c7213f5fccaa5b267f1ca469f5546563a04ef354
flynn-chen <flynn.chen@yale.edu>
2022-02-23 01:59:41 -0500

    adde stochastic volatility

--- src/systemic_risk/BrownianBridgeSimStoVol.py
+++ src/systemic_risk/BrownianBridgeSimStoVol.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot
 
-class BrownianBridgeSim():
+class BrownianBridgeSimStoVol():
     
     """
     Simulating multivariate time-series between two available time points using brownian bridges.
@@ -24,15 +24,18 @@ class BrownianBridgeSim():
             * random seed
     """
     
-    def __init__(self, data, N=24, window_size=60, seed = 0):
+    def __init__(self, data, N=24, window_size=60, seed = 0, std_threshold=3):
         self.data = data
         self.N = N
         self.window_size = window_size
         self.seed = seed
+        self.std_threshold = std_threshold
         np.random.seed(seed)
         
         self.indexer = pd.api.indexers.FixedForwardWindowIndexer(window_size=window_size)
         self.rolling_std = self.data.rolling(window=self.indexer, 
+                                             min_periods=1).std().iloc[:(self.data.shape[0] - 1), :]
+        self.rolling_mean = self.data.rolling(window=self.indexer, 
                                              min_periods=1).std().iloc[:(self.data.shape[0] - 1), :]
         self.M = (self.data.shape[0] - 1) * self.data.shape[1]
 
@@ -49,6 +52,22 @@ class BrownianBridgeSim():
         B[:, -1] = 0
         return B
     
+    def stochastic_volatility(self, repeated_prior_std):
+         
+        # Find the std of the next moves
+        next_move_std = self.data.iloc[self.window_size:, :].to_numpy() - \
+                        self.rolling_mean.iloc[:self.rolling_mean.shape[0] - (self.window_size - 1), :].to_numpy()
+        next_move_std = np.abs(next_move_std) / self.rolling_std.iloc[:self.rolling_std.shape[0] - (self.window_size - 1), :].to_numpy()
+
+        # Calculate the proportion of days with std above [std_threshold]
+        proportion_of_jumps = np.sum(next_move_std > self.std_threshold) / np.prod(self.rolling_std.iloc[:self.rolling_std.shape[0] - (self.window_size - 1), :].shape)
+
+        # Update std on random days based on sampled highly volatile days in the past
+        new_std_mask = np.random.uniform(size = repeated_prior_std.shape) < proportion_of_jumps
+        repeated_prior_std[new_std_mask] = np.random.choice(next_move_std[next_move_std > 3], size=repeated_prior_std[new_std_mask].shape)
+        
+        return repeated_prior_std
+    
     def simulate(self):
         
         # Simulate a standard brownian motion
@@ -57,7 +76,9 @@ class BrownianBridgeSim():
         # Rescale
         prior_std = self.rolling_std.to_numpy().flatten()
         repeated_prior_std = np.tile(prior_std, (self.simulated_data.shape[0], 1))
+        repeated_prior_std = self.stochastic_volatility(repeated_prior_std)
         self.simulated_data *= repeated_prior_std
+        
 
         # Shift
         start_price = self.data.iloc[0:-1, :].to_numpy().flatten()
@@ -69,4 +90,5 @@ class BrownianBridgeSim():
         self.simulated_data = self.simulated_data.reshape(self.N, (self.data.shape[0] - 1), self.data.shape[1])
         self.simulated_data = np.swapaxes(self.simulated_data,0,1)
         self.simulated_data = self.simulated_data.reshape(self.N * (self.data.shape[0] - 1), self.data.shape[1])
+        
         return self.simulated_data
